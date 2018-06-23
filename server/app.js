ccc8b644278c497ff6b628cb68b2935ff92fddd5
chenyunfei <438376555@qq.com>
2018-06-23 21:52:40 +0800

    '新增了xmlhttprequest、jsonp实现代码'

---
+++ server/app.js
@@ -0,0 +1,6 @@
+const http = require(`http`);
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text-plain' });
+    const result = `handleResponse(${JSON.stringify({ name: 'cyf' })})`;
+    response.end(result);
+}).listen(3000);
