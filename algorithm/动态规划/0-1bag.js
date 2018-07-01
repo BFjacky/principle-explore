e5c0e6c41b8d0329f647bd449055201c1415f398
chenyunfei <438376555@qq.com>
2018-07-01 23:14:43 +0800

    '新增0-1背包的动态规划版实现'

--- algorithm/动态规划/0-1bag.js
+++ algorithm/动态规划/0-1bag.js
@@ -7,7 +7,12 @@
 const V = 10;
 const objs = [{ p: 3, v: 3 }, { p: 5, v: 10 }, { p: 3, v: 2 }, { p: 7, v: 11 }, { p: 1, v: 1 }];
 
-const bag = [];
+
+//动态规划核心，保存已经求解出来的问题不再用递归实现，直接在数组中得到结果
+const max = [];
+for (const obj of objs) {
+    max.push({ in: undefined, out: undefined })
+}
 
 //该函数的意义:求出一个给定的v和objs所对应的最优解
 const solute = function (V, objs) {
@@ -26,21 +31,28 @@ const solute = function (V, objs) {
     //在objs中找出第一个能放进去的物品
     for (let i = 0; i < objs.length; i++) {
         if (objs[i].v <= V) {
-            //将objs[i]放进去的最优解
-            const newObjs = objs.slice(0, i).concat(objs.slice(i + 1))
-            const res1 = solute(V - objs[i].v, newObjs)
-            //不将objs[i]放进去的最优解
-            const res2 = solute(V, newObjs)
+            if (max[i].in === undefined && max[i].out === undefined) {
+                //将objs[i]放进去的最优解
+                const newObjs = objs.slice(0, i).concat(objs.slice(i + 1))
+                const res1 = solute(V - objs[i].v, newObjs)
+                //不将objs[i]放进去的最优解
+                const res2 = solute(V, newObjs)
+                //将结果存入到max数组中
+                max[i].in = res1;
+                max[i].out = res2;
+            }
 
-            if (res1 + objs[i].p > res2) {
-                return res1 + objs[i].p;
+
+            if (max[i].in + objs[i].p > max[i].out) {
+                return max[i].in + objs[i].p;
             }
             else {
-                return res2;
+                return max[i].out;
             }
         }
     }
 
+    //有两件物品以上，未找到物品重量小于背包剩余容量的物品
     return 0;
 }
 
