4457ab3f93ef685c231c7f14f10a364a59001e4a
chenyunfei <438376555@qq.com>
2018-07-02 18:41:48 +0800

    为jsonp添加了promise

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const http = require(`http`);
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text-plain' });
-    const result = `handleResponse(${JSON.stringify({ name: 'cyf' })})`;
+    const result = `returnRes(${JSON.stringify({ name: 'cyf' })})`;
     response.end(result);
 }).listen(3000);
